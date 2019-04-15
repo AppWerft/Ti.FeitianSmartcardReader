95437b61b59da1fb73cd7c6c86b61a3b3a06843a
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2019-04-15 12:36:38 +0200

    openDevice added

--- android/src/de/appwerft/feitian/FeitianModule.java
+++ android/src/de/appwerft/feitian/FeitianModule.java
@@ -213,7 +213,6 @@ public class FeitianModule extends KrollModule {
 				String[] readerNames;
 				try {
 					Log.d(LCAT, "try readerOpen() " );
-					Log.d(LCAT,ftReader.readerGetFirmwareVersion());
 					//ftReader.readerPowerOn(0);
 					readerNames = ftReader.readerOpen(dev);
 					Log.d(LCAT, "readerOpened " +readerNames.length);
