3021349d8f6394b41a59ff7b68eac49a12d06f49
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2020-03-13 16:28:10 +0100

    readme

--- android/src/de/appwerft/feitian/HealthCardAsyncAdapter.java
+++ android/src/de/appwerft/feitian/HealthCardAsyncAdapter.java
@@ -39,23 +39,11 @@ public class HealthCardAsyncAdapter extends AsyncTask<Void, Void, byte[]> {
 	}
 
 	private String getPatientData() throws IOException {
-		// Create read command for the first two bytes of patient file.
-		// Read first two byte of patient data. These contain the length of the PD file.
-		// byte[] data = transmit(APDU.readData(0,2));
-
-		// Since the two bytes are included themselves those two bytes are subtracted
-		// from the length.
-		// int pdLength = 8*data[0]+data[1]-2;
 		byte[] READ_PD = new byte[] { 0x00, (byte) 0xb0, (byte) (0x80 + 0x01), 0x00, 0x00, 0x00, 0x00 };
-		Log.d(LCAT, "READ_PD try to send = "+ Utility.bytes2HexStr(READ_PD));
 		byte[] pd;
 		try {
-			//pd = transmit(READ_PD);
 			transmit(APDU.getCmd(APDU.SELECT_FILE_PD));
 			pd = transmit(APDU.getCmd(APDU.READ_PD));
-			Log.d(LCAT, "READ_PD sent");
-			Log.d(LCAT, "Length of raw pd =" + pd.length);
-			Log.d(LCAT, "first 2 bytes =" + pd[0] + pd[1]);
 			int pdLength = ((pd[0] & 0xff) << 8) | (pd[1] & 0xff);
 			Log.d(LCAT, "Length of pd =" + pdLength);
 			byte[] pdDataCompressed = new byte[pdLength];
@@ -70,7 +58,7 @@ public class HealthCardAsyncAdapter extends AsyncTask<Void, Void, byte[]> {
 	}
 
 	private byte[] transmit(byte[] apdu) throws FTException {
-			Log.d(LCAT, "SEND: " +Utility.bytes2HexStr(apdu));
+			Log.d(LCAT, "SEND: " + Utility.bytes2HexStr(apdu));
 			byte[] res = ftReader.readerXfr(0, apdu);
 			Log.d(LCAT, "RCV: " + Utility.bytes2HexStr(res));
 			return res;
